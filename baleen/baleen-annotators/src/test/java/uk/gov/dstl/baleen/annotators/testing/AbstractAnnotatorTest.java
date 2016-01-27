7b92204194e5b4d0fe44b372917a54989a7be9d5
chrisflatley <chrisflatley@tenode.com>
2016-01-27 10:42:52 +0000

    Pull general test functions in to the AnnotationTestBase
    Create the MultiAnnotatorTest for more complex analysis engines

--- baleen/baleen-annotators/src/test/java/uk/gov/dstl/baleen/annotators/testing/AbstractAnnotatorTest.java
+++ baleen/baleen-annotators/src/test/java/uk/gov/dstl/baleen/annotators/testing/AbstractAnnotatorTest.java
@@ -1,39 +1,37 @@
 //Dstl (c) Crown Copyright 2015
 package uk.gov.dstl.baleen.annotators.testing;
 
-import static org.junit.Assert.assertEquals;
-
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
-import org.apache.uima.fit.util.JCasUtil;
-import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
-import org.apache.uima.jcas.tcas.DocumentAnnotation;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import uk.gov.dstl.baleen.uima.BaleenAnnotator;
 
-/** A base type for single annotator testing, with helper functions.
+/**
+ * A base type for single annotator testing, with helper functions.
  *
  * Similar in nature to {@link AnnotatorTestBase} but assumes testing a single annotator.
  *
- * 
+ *
  *
  */
 public class AbstractAnnotatorTest extends AnnotatorTestBase {
 
-	private Class<? extends BaleenAnnotator> annotatorClass;
+	private final Class<? extends BaleenAnnotator> annotatorClass;
 
-
-	/** New instance, which will test the supplied annotator class.
+	/**
+	 * New instance, which will test the supplied annotator class.
+	 * 
 	 * @param annotatorClass
 	 */
 	public AbstractAnnotatorTest(Class<? extends BaleenAnnotator> annotatorClass) {
 		this.annotatorClass = annotatorClass;
 	}
 
-	/** Get an analysis engine for the
+	/**
+	 * Get an analysis engine for the
+	 * 
 	 * @return
 	 * @throws ResourceInitializationException
 	 */
@@ -41,8 +39,11 @@ public class AbstractAnnotatorTest extends AnnotatorTestBase {
 		return AnalysisEngineFactory.createEngine(annotatorClass);
 	}
 
-	/** Get an analysis engine for the
-	 * @param args name-value pairs
+	/**
+	 * Get an analysis engine for the
+	 * 
+	 * @param args
+	 *            name-value pairs
 	 * @return
 	 * @throws ResourceInitializationException
 	 */
@@ -50,8 +51,9 @@ public class AbstractAnnotatorTest extends AnnotatorTestBase {
 		return AnalysisEngineFactory.createEngine(annotatorClass, args);
 	}
 
-
-	/** process the {@link AnnotatorTestBase}'s jCas with the annotator.
+	/**
+	 * process the {@link AnnotatorTestBase}'s jCas with the annotator.
+	 * 
 	 * @throws ResourceInitializationException
 	 * @throws AnalysisEngineProcessException
 	 */
@@ -61,8 +63,12 @@ public class AbstractAnnotatorTest extends AnnotatorTestBase {
 		analysisEngine.destroy();
 	}
 
-	/** process the {@link AnnotatorTestBase}'s jCas with the annotator (configured with the args provided).
-	 * @param args name-value pairs
+	/**
+	 * process the {@link AnnotatorTestBase}'s jCas with the annotator (configured with the args
+	 * provided).
+	 * 
+	 * @param args
+	 *            name-value pairs
 	 * @throws ResourceInitializationException
 	 * @throws AnalysisEngineProcessException
 	 */
@@ -70,42 +76,5 @@ public class AbstractAnnotatorTest extends AnnotatorTestBase {
 		AnalysisEngine analysisEngine = getAnalysisEngine(args);
 		analysisEngine.process(jCas);
 		analysisEngine.destroy();
-	}
-
-
-	/** Process the {@link AnnotatorTestBase} jCas object looking for supplied annotations.
-	 *
-	 * You must call processJcas (or equivalent first).
-	 *
-	 * @param size total number of annotations to expect of this type
-	 * @param annotationClass the annotation class to look for
-	 * @param annotations (a subset of) annotations to test
-	 * @throws AnalysisEngineProcessException
-	 * @throws ResourceInitializationException
-	 */
-	@SafeVarargs
-	protected final <T extends Annotation> void assertAnnotations(int size, Class<T> annotationClass, TestAnnotation<T>... annotations) throws AnalysisEngineProcessException, ResourceInitializationException {
-
-		assertEquals(size, JCasUtil.select(jCas, annotationClass).size());
-
-		for(TestAnnotation<T> a : annotations) {
-			T t = JCasUtil.selectByIndex(jCas, annotationClass, a.getIndex());
-			a.validate(t);
-		}
-	}
-
-	/** Get the document annotation from the default jCas.
-	 * @return documentation annotation
-	 */
-	protected DocumentAnnotation getDocumentAnnotation() {
-		return getDocumentAnnotation(jCas);
-	}
-
-	/** Get the document annotation from a jCas.
-	 * @param jCas
-	 * @return documentation annotation
-	 */
-	protected DocumentAnnotation getDocumentAnnotation(JCas jCas) {
-		return (DocumentAnnotation) jCas.getDocumentAnnotationFs();
 	}
 }
